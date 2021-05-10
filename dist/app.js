faab04ead48d6d363604ee5984a7a4dfd4909d89
Mattia Bocchino <pmattiab@live.it>
2021-05-10 15:43:33 +0200

    terminato header con navbar

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,13 @@
   \********************/
 /***/ (() => {
 
-
+var app = new Vue({
+  el: "#root",
+  data: {
+    headerLinks: ["home", "about us", "facilities", "membership", "testimonials", "blog"],
+    activeHeaderLink: 0
+  }
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,9 @@
+var app = new Vue ({
+
+    el: "#root",
+    data: {
+        headerLinks: ["home", "about us", "facilities", "membership", "testimonials", "blog"],
+        activeHeaderLink: 0
+    }
+
+});
