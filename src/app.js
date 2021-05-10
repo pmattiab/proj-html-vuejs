c73f75838c5f85293fc429292b5371ac0eb426c3
Mattia Bocchino <pmattiab@live.it>
2021-05-10 17:37:01 +0200

    risolto errore terminale

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-var app = new Vue ({
+// var app = new Vue ({
 
-    el: "#root",
-    data: {
-        headerLinks: ["home", "about us", "facilities", "membership", "testimonials", "blog"],
-        activeHeaderLink: 0
-    }
+//     el: "#root",
+//     data: {
+//         headerLinks: ["home", "about us", "facilities", "membership", "testimonials", "blog"],
+//         activeHeaderLink: 0
+//     }
 
-});
+// });
