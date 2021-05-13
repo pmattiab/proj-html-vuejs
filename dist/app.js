563704671e06fe73b3942270601000298b4b5dde
Mattia Bocchino <pmattiab@live.it>
2021-05-13 12:27:24 +0200

    footer terminato + nuove mixins

--- dist/app.js
+++ dist/app.js
@@ -8,11 +8,73 @@
 /***/ (() => {
 
 var app = new Vue({
+  // elemento vue js in html
   el: "#root",
+  // data
   data: {
+    // link per la nav bar nell'header
     headerLinks: ["home", "about us", "facilities", "membership", "testimonials", "blog"],
-    activeHeaderLink: 0
+    // link attivo nella nav bar nell'header
+    activeHeaderLink: 0,
+    // menu nel footer superiore
+    footerMenu: [{
+      listTitle: "Come find us",
+      listElement: [{
+        text: "12345 North Main Street",
+        "class": "contacts"
+      }, {
+        text: "New York, NY 555555",
+        "class": "contacts"
+      }, {
+        text: "Phone: 1.800.555.6789",
+        "class": "contacts"
+      }, {
+        text: "Email: info@your-domain.com",
+        "class": "contacts"
+      }]
+    }, {
+      listTitle: "Recent Posts",
+      listElement: [{
+        text: "Train with free weights or your body weight?",
+        "class": "post"
+      }, {
+        text: "Nutritional advice that will keep you training",
+        "class": "post"
+      }]
+    }, {
+      listTitle: "Opening Times",
+      listElement: [{
+        text: "Weekdays Monday - Friday",
+        "class": "days"
+      }, {
+        text: "09:00 - 19:00",
+        "class": "schedule"
+      }, {
+        text: "Weekends Saturday - Sunday",
+        "class": "days"
+      }, {
+        text: " 09:00 - 21:00",
+        "class": "schedule"
+      }]
+    }],
+    // menu copyright nel footer inferiore
+    footerCopyright: ["© Copyright 2012 - 2020", "Avada Theme by Theme Fusion", "All Rights Reserved", "Powered by WordPress"],
+    // bottoni social nel footer inferiore
+    footerSocial: [{
+      social: "facebook",
+      "class": "fa-facebook-f",
+      url: "https://www.facebook.com"
+    }, {
+      social: "twitter",
+      "class": "fa-twitter",
+      url: "https://www.twitter.com"
+    }, {
+      social: "instagram",
+      "class": "fa-instagram",
+      url: "https://www.instagram.com"
+    }]
   },
+  // methods (funzioni)
   methods: {
     changeActivePage: function changeActivePage(index) {
       this.activeHeaderLink = index;
